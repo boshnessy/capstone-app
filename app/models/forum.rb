7e9aa3776206b34b3458339cda2af421797cc11c
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-04-08 14:22:33 -0500

    create table associations

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,2 +1,5 @@
 class Forum < ApplicationRecord
+  belongs_to :event
+  belongs_to :user
+  has_many :reports
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,4 @@
 class Report < ApplicationRecord
+  belongs_to :forum
+  belongs_to :user
 end
