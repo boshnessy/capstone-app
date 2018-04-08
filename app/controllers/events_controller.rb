ee1282e1a55f65e5f022d15b5a79985c4d47f026
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-04-08 16:14:29 -0500

    add create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,31 @@ class EventsController < ApplicationController
     events = Event.all
     render json: events.as_json
   end
+
+  def show
+    artist = params[:artist]
+    events = Event.where(artist: artist)
+
+    # venue = params[:venue]
+    # events = Event.where(venue: venue)
+
+    # Event.where("date LIKE ?", "%Apr%")
+
+    render json: event.as_json
+  end
+
+  def create
+    event = Event.new(
+      date: params[:date],
+      artist: params[:artist],
+      venue: params[:venue],
+      city: params[:city]
+    )
+
+    if event.save
+      render json: event.as_json
+    else
+      render json: {errors: event.errors.full_messages}
+    end
+  end
 end
--- frontend.rb
+++ frontend.rb
@@ -1,22 +1,43 @@
 require 'unirest'
 
-p "Enter email"
-input_email = gets.chomp
-p "Enter password"
-input_password = gets.chomp
+# LOG IN
+# p "Enter email"
+# input_email = gets.chomp
+# p "Enter password"
+# input_password = gets.chomp
 
-response = Unirest.post("localhost:3000/user_token",
+# response = Unirest.post("localhost:3000/user_token",
+#   parameters: {
+#     auth: {
+#       email: input_email,
+#       password: input_password
+#     }
+#   }
+# )
+
+# p response.body
+
+# jwt = response.body["jwt"]
+# Unirest.default_header("Authorization", "Bearer #{jwt}")
+
+# p jwt
+
+# SHOW ACTION
+# p "Enter search term"
+# search = gets.chomp
+
+# response = Unirest.get("localhost:3000/events?search=#{search}")
+
+# p response.body
+
+# CREATE ACTION
+response = Unirest.post("localhost:3000/events", 
   parameters: {
-    auth: {
-      email: input_email,
-      password: input_password
-    }
+    date: "Apr 8, 2018",
+    artist: "me",
+    venue: "condo",
+    city: "Chicago"
   }
 )
 
 p response.body
-
-jwt = response.body["jwt"]
-Unirest.default_header("Authorization", "Bearer #{jwt}")
-
-p jwt
